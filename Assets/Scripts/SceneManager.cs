a891bbc35ad017d8c1814028edb3f7e1dd8dc88d
Fernando de Rojas <frs@unusualwonder.com>
2016-10-05 14:28:51 +0200

    [MainMenu] Borrador completado

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,12 @@
+﻿
+
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{ 
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+
+}
