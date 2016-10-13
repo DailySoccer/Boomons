1a944dd4de3d63c8df178751bd108bf366596cb6
Fernando de Rojas <frs@unusualwonder.com>
2016-10-13 10:15:56 +0200

    [MainMenu] - Title

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -25,7 +25,6 @@ public class GameManager : Singleton<GameManager>
 		Boomon = boomonGo.GetComponent<BoomonController>();
 	}
 
-
 	#endregion
 
 	//=======================================================
