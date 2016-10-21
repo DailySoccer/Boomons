7bfe18fe4bcd87ba61f542c0e099538304e5f4fc
Silvio_Unusual <sjimenez@unusualwonder.com>
2016-10-21 16:43:04 +0200

    Full parent menu flow added. (lacks QR processing)

---
+++ Assets/Scripts/ResultQR.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ResultQR : MonoBehaviour {
+
+	public RectTransform CorrectPanel;
+	public RectTransform IncorrectPanel;
+	public RectTransform CorrectBackMenu;
+	public RectTransform IncorrectBackMenu;
+	public bool Result;
+	// Use this for initialization
+	void Start () {
+		_initialized = CorrectPanel != null && IncorrectPanel != null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetResult(bool result)
+	{
+		if (_initialized)
+		{
+			Result = result;
+			CorrectPanel.gameObject.SetActive(result);
+			IncorrectPanel.gameObject.SetActive(!result);
+		}
+	}
+
+	public void GoBackToMenu()
+	{
+		if (_initialized)
+		{
+			CorrectBackMenu.gameObject.SetActive(Result);
+			IncorrectBackMenu.gameObject.SetActive(!Result);
+		}
+	}
+
+	private bool _initialized;
+}
