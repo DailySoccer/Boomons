99fee72fca37f0a310b4a5f159c2dbb70beecc4d
Silvio_Unusual <sjimenez@unusualwonder.com>
2016-10-24 15:27:27 +0200

    Splash screen, main menu and complete menu flow.

---
+++ Assets/Scripts/Managers/CustomSceneManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CustomSceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GoToMainMenu()
+	{
+		SceneManager.LoadScene("MainMenuScene");
+	}
+
+	public void GoToParentsMenu()
+	{
+		SceneManager.LoadScene("ParentsMenuScene");
+	}
+
+	public void GoToPlay()
+	{
+		SceneManager.LoadScene("");
+	}
+}
