74c713ea863c134e6fda23eddfbafc608de28b2e
Silvio_Unusual <sjimenez@unusualwonder.com>
2016-10-25 16:45:39 +0200

    Fixed a little bug, now size is taken from sprite not from texture.

--- Assets/Scripts/UI/AdjustToScreen.cs
+++ Assets/Scripts/UI/AdjustToScreen.cs
@@ -9,10 +9,10 @@ public class AdjustToScreen : MonoBehaviour {
 		RectTransform mine = gameObject.GetComponent<RectTransform>();
 		if (mine != null && refTexture != null)
 		{
-			float ratioTex = refTexture.texture.width * 1.0f / refTexture.texture.height;
+			float ratioTex = refTexture.rect.width * 1.0f / refTexture.rect.height;
 			float ratioScreen = Screen.width * 1.0f / Screen.height;
 			float finalHeight = ratioScreen > ratioTex ? Screen.height : Screen.width / ratioTex;
-			mine.sizeDelta = new Vector2(finalHeight, finalHeight*ratioTex);
+			mine.sizeDelta = new Vector2(finalHeight * ratioTex, finalHeight);
 		}
 	}
 	
