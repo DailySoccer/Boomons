5c134c0b9e41f9edfba5daa9e1573a7e940052a1
Fernando de Rojas <frs@unusualwonder.com>
2016-10-27 22:32:21 +0200

    WIP

---
+++ Assets/Cutscene.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Cutscene : MonoBehaviour
+{
+
+	#region Mono
+
+	private void Awake()
+	{
+		_animator = GetComponent<Animator>();
+		_driver = GetComponentInChildren<CutsceneDriver>();
+		_game = MetaManager.Instance.GetManager<GameManager>();
+	}
+
+	private void OnDestroy()
+	{
+		_game = null;
+		_animator = null;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject != _game.Player)
+			return;
+
+		_animator.SetTrigger(_playTriggerName);
+		
+	}
+
+	#endregion
+
+	//==================================================
+
+	#region Private Fields
+
+	
+	[SerializeField] private string _playTriggerName = "Play";
+	[SerializeField] private string _playerTag = "Player";
+
+	private GameManager _game;
+	private Animator _animator;
+	#endregion
+
+}
