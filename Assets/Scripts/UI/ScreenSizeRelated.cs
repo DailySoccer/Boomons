b975d4bb724d4c57f089d1e6252640f82e79db87
Silvio_Unusual <sjimenez@unusualwonder.com>
2016-10-31 12:06:49 +0100

    Fixed menu buttons size depending on screen size.

---
+++ Assets/Scripts/UI/ScreenSizeRelated.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenSizeRelated : MonoBehaviour {
+
+	[SerializeField]
+	[Range(0, 1)]
+	private float Percentage = 0.2f;
+	// Use this for initialization
+	void Start () {
+		float shortest = Mathf.Min(Screen.width, Screen.height);
+		RectTransform myRect = GetComponent<RectTransform>();
+		if (myRect != null)
+		{
+			myRect.sizeDelta = Vector2.one * shortest * Percentage;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
