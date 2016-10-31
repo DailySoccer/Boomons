bf2480d2470128561b9d54662c1d45e4a9d38271
Silvio_Unusual <sjimenez@unusualwonder.com>
2016-10-31 12:33:06 +0100

    More generic script for screen related UI elements. Parent button has now screen related size.

--- Assets/Scripts/UI/ScreenSizeRelated.cs
+++ Assets/Scripts/UI/ScreenSizeRelated.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ScreenSizeRelated : MonoBehaviour {
@@ -8,11 +9,13 @@ public class ScreenSizeRelated : MonoBehaviour {
 	private float Percentage = 0.2f;
 	// Use this for initialization
 	void Start () {
+		Sprite refSprite = GetComponentInChildren<Image>().sprite;
+		float ratioSprite = refSprite != null ? refSprite.rect.width / refSprite.rect.height : 1;
 		float shortest = Mathf.Min(Screen.width, Screen.height);
 		RectTransform myRect = GetComponent<RectTransform>();
 		if (myRect != null)
 		{
-			myRect.sizeDelta = Vector2.one * shortest * Percentage;
+			myRect.sizeDelta = new Vector2(ratioSprite , 1) * shortest * Percentage;
 		}
 	}
 	
