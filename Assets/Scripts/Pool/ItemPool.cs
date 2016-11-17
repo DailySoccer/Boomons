f09b22292912b144df074beaeb2f17c61cf6e6de
Silvio_Unusual <sjimenez@unusualwonder.com>
2016-11-17 10:45:00 +0100

    Modified directory look up method.

--- Assets/Scripts/Pool/ItemPool.cs
+++ Assets/Scripts/Pool/ItemPool.cs
@@ -11,30 +11,42 @@ public class ItemPool : MonoBehaviour {
 	void Start () {
 		if (!string.IsNullOrEmpty(directoryLocation))
 		{
-			DirectoryInfo metaFolder = new DirectoryInfo(Application.dataPath + FolderSeparator + directoryLocation);
-			FileInfo[] fileList = metaFolder.GetFiles();
-			foreach (FileInfo fileInf in fileList)
+			//Read locations available
+			Transform[] childrenTrans = transform.GetComponentsInChildren<Transform>();
+			foreach (Transform tf in childrenTrans)
 			{
-				string prefabName = fileInf.FullName;
-				if (prefabName.Contains(".prefab") && !prefabName.Contains(".meta"))
+				if (tf != transform)
 				{
-					int lowLimit = prefabName.IndexOf("Resources") + 10;
-					int highLimit = prefabName.IndexOf(".prefab");
-					prefabs.Add(Resources.Load<GameObject>(prefabName.Substring(lowLimit, highLimit - lowLimit)));
+					_locations.Add(tf);
 				}
+			}
+			//Load prefabs (as much as locations available)
+			Object[] prefabList = Resources.LoadAll(directoryLocation, typeof(GameObject));
+			foreach (Object obj in prefabList)
+			{
+				if (_prefabs.Count < _locations.Count)
+				{
+					GameObject go = obj as GameObject;
+					if (go != null)
+					{
+						_prefabs.Add(go);
+					}
+				}
+			}
+			//Generate prefabs on available locations
+			for (int i = 0; i < _prefabs.Count; ++i)
+			{
+				int index = Random.Range(0,_locations.Count);
+				Transform nextParent = _locations[index];
+				_locations.RemoveAt(index);
+				GameObject newGenerated = GameObject.Instantiate<GameObject>(_prefabs[i]);
+				newGenerated.transform.SetParent(nextParent);
+				newGenerated.transform.localPosition = Vector3.zero;
+				newGenerated.transform.localRotation = Quaternion.identity;
 			}
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-	private List<GameObject> prefabs = new List<GameObject>();
-#if UNITY_EDITOR_WIN
-	private const string FolderSeparator = "\\";
-#else
-	private const string FolderSeparator = "/";
-#endif
+	private List<GameObject> _prefabs = new List<GameObject>();
+	private List<Transform> _locations = new List<Transform>();
 }
